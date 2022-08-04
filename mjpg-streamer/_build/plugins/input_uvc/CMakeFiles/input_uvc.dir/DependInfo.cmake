c92a997dec710ad691194b278a5dbbf691437a9e
Krzysztof Waszak <kwaszak67@gmail.com>
2022-08-04 22:11:27 +0200

    use experimental mjpg-streamer

---
+++ mjpg-streamer/_build/plugins/input_uvc/CMakeFiles/input_uvc.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/plugins/input_uvc/dynctrl.c" "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/_build/plugins/input_uvc/CMakeFiles/input_uvc.dir/dynctrl.c.o"
+  "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/plugins/input_uvc/input_uvc.c" "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/_build/plugins/input_uvc/CMakeFiles/input_uvc.dir/input_uvc.c.o"
+  "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/plugins/input_uvc/jpeg_utils.c" "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/_build/plugins/input_uvc/CMakeFiles/input_uvc.dir/jpeg_utils.c.o"
+  "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/plugins/input_uvc/v4l2uvc.c" "/home/waszak/Desktop/picar/SunFounder_PiCar-V/mjpg-streamer/_build/plugins/input_uvc/CMakeFiles/input_uvc.dir/v4l2uvc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GIT_HASH=\"6b8bfc4abb6da4b486d1d59c4c7c422ae14832da\""
+  "LINUX"
+  "USE_LIBV4L2"
+  "_GNU_SOURCE"
+  "input_uvc_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
